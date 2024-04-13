5a0613fe480db28f7b9f2d1d6de8fd189c6c0482
Bisal-Shafiq <bisalshafiq4@gmail.com>
2024-04-13 17:17:46 +0500

    beta ATM

---
+++ index.ts
@@ -0,0 +1,78 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+let myBalance = 100000; // Doller
+let myPin = 123;
+
+let myAnswer = await inquirer.prompt(
+    [
+        {
+        name: "pin",
+        message: "Enter your Pin:",
+        type: "number",
+        }
+    ]
+);
+
+// 123 === 1234 - false
+
+if (myAnswer.pin === myPin) {
+
+    console.log("Correst Pin Code!!!");
+
+    let operationAnswer = await inquirer.prompt(
+        [
+            {
+        name: "operation",
+        message: "pelect select option",
+        type: "list",
+        choices: ["withdraw" , "check balance" , "past cash"],
+            }
+        ]
+    );
+
+    console.log(operationAnswer);
+
+    if (operationAnswer.operation === "withdraw"){
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "Enter your Amount:",
+                    type: "number",
+                }
+            ]
+        );
+
+        if (myBalance -= amountAns.amount){
+            myBalance <= amountAns.amount;
+            console.log(`Your remaining balance is ${myBalance}`);
+
+        }else {
+            console.log(`insuficent balance. you can only withdaw up to ${myBalance}`);
+            }
+    }   else if(operationAnswer.operation === "check balance") {
+        console.log(`Your balance is ${myBalance}`);
+
+        } else if (operationAnswer.operation === "past cash") {
+            let oeratortAns = await inquirer.prompt(
+                [
+                    {
+                        name: "operator",
+                        message: "Enter your Amount:",
+                        type: "list",
+                        choices: [10000, 20000, 30000],
+                    }
+                ]
+            );
+
+            console.log("withdraw amounte successfully")
+
+
+            }
+}
+else {
+    console.log("Incorrect Pin Number");
+    }
